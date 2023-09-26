22998ed867a4a2878ab57b8537b4fbb2ce6a4da0
Sergey Gavrilov <serggavril00vs@gmail.com>
2023-09-26 20:48:12 +0700

    add

---
+++ themes/wto/assets/js/custom.js
@@ -0,0 +1,23 @@
+(function() {
+    const selectors = {
+        section: '.s-section_pd_home',
+        home: '.b-home-wrap',
+        list: '[data-list]',
+    }
+
+    const $section = document.querySelector(selectors.section);
+    const $modal = document.querySelector(selectors.modal);
+    const $home = $section.querySelector(selectors.home);
+    const $list = $section.querySelector(selectors.list);
+    const $items = Array.from($list.children);
+
+    $home.addEventListener('click', function(event) {
+        const $btn = event.target.closest('[data-item]')
+        if ($btn) {
+            const itemIndex = Number($btn.dataset.item);
+            $items.forEach(function ($item, index) {
+                $item.classList.toggle('active', index === itemIndex - 1);
+            })
+        }
+    })
+})()
