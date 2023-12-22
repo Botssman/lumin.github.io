1b01f5d49756cfc2f120c51a812fb57b3125a5ea
Sergey Gavrilov <serggavril00vs@gmail.com>
2023-12-23 00:09:31 +0700

    add parol

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.yandex.ru'),
+            'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('maxbotssman@yandex.ru'),
+            'password' => env('escymdgbjwknpgty'),
             'timeout' => null,
             'auth_mode' => null,
         ],
